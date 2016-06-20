073fb6d64435f0fc4314565eb39e6fe5bf15d316
abdulhamied77 <abdulhamied77@gmail.com>
2016-06-21 06:29:07 +0700

    Add files via upload

---
+++ nof12.js
@@ -0,0 +1,26 @@
+
+
+//////////F12 disable code////////////////////////
+    document.onkeypress = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+           //alert('No F-12');
+            return false;
+        }
+    }
+    document.onmousedown = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+            //alert('No F-keys');
+            return false;
+        }
+    }
+document.onkeydown = function (event) {
+        event = (event || window.event);
+        if (event.keyCode == 123) {
+            //alert('No F-keys');
+            return false;
+        }
+    }
+
+    // sumber : http://www.enjin.com/forums/m/10826/viewthread/4760201-protect-your-content-disable-rightclick-f12-keys
---
+++ norightclick.js
@@ -0,0 +1,16 @@
+
+<!-- 
+//Disable right click script 
+//visit http://www.rainbow.arch.scriptmania.com/scripts/ 
+var message="Sorry, right-click has been disabled"; 
+/////////////////////////////////// 
+function clickIE() {if (document.all) {(message);return false;}} 
+function clickNS(e) {if 
+(document.layers||(document.getElementById&&!document.all)) { 
+if (e.which==2||e.which==3) {(message);return false;}}} 
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;} 
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;} 
+document.oncontextmenu=new Function("return false") 
+// --> 
+// sumber : http://www.enjin.com/forums/m/10826/viewthread/4760201-protect-your-content-disable-rightclick-f12-keys
